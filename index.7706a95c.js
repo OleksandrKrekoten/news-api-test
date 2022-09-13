5086ccd1dfa575949cf14f064ee8e3746d279d88
OleksandrKrekoten <98402004+OleksandrKrekoten@users.noreply.github.com>
2022-09-13 10:55:10 +0000

    Deploying to gh-pages from @ OleksandrKrekoten/news-api-test@436a08cb090fd442b3f93f65399b1995bcd4fb3b 🚀

--- index.7706a95c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".js-searchForm"),document.querySelector(".form-control"),document.querySelector(".submitBtn"),document.querySelector(".gallery-news");
-//# sourceMappingURL=index.7706a95c.js.map
--- index.d3cd2cdb.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".js-searchForm"),document.querySelector(".form-control"),document.querySelector(".submitBtn"),document.querySelector(".gallery-news");
-//# sourceMappingURL=index.d3cd2cdb.js.map
