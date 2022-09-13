cdfdfdc0b9a775c6e1726b1e215699aee9b8bdf9
Oleksandr <alexkrekoten11@gmail.com>
2022-09-13 09:18:54 +0300

    parsel

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+
+const refs = {
+    form: document.querySelector('.js-searchForm'), 
+    input: document.querySelector('.form-control'),
+    submitBtn: document.querySelector('.submitBtn'),
+    galleryNews: document.querySelector('.gallery-news')
+}
